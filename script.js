e8fb8bdcda52637713c817140ed95bbbaac02463
Anastasiya <nastyadorul@gmail.com>
2021-10-18 02:03:20 +0300

    final

--- script.js
+++ script.js
@@ -1,199 +1,163 @@
 let ls = [51,56,58,59,61];
-let k=3;
+
+let copyLs=ls.slice();
 let result=[];
-let preresult=[];
-let copied=ls.slice();
+let rest=[];
+let finalRest=[];
 let sum=[];
 
 
+let i,p,w,k,arg;
 
+function getSecondArg(arg){
 
-const chooseDistance = (t, k, ls) => {
-
-    function getCombination(copied,k){
-        let previous=[];
-        for(j=k;j>1;j--){
-            previous=(result[result.length - 1]).join(' ').split(",");
-            next=previous.map(i=>+i)
-        
-            for(i=2;i<copied.length;i++){
-                if(next.includes(copied[i])){
-                    next.splice(2,next.indexOf(copied[i]));
-                }
-                else if(next.length!==3){
-                    next.push(copied[i]);
-                }   
-            }
-            result.push([next]);
-        }return result;
-    }
-    
-    function SaveFirst(copied,preresult,result){
-        preresult=[copied[0],copied[1],copied[2]];
-        return result.push([preresult]);
-    }
-
-    
-    function getFinalResult(value,t){
-        value.map((item)=>{
-            item=[].concat.apply([],item);
-            item=(item.reduce((a,b)=>a+b,0))-t;
-            sum.push(item);
-        });
-        let negative = sum.filter((elem)=>{
-            return elem<0;
-        });
-        if(negative.length==0){
-            console.log("There is no acceptable result");
+    console.log(rest=ls);
+        for(let x=0;x<arg.length;x++){
+            rest=rest.filter((elem)=>{
+            return elem!==arg[x];
+            })
+        finalRest=rest;   
         }
-        else
-        {
-            let max=Math.max.apply(null, negative);
-            let index=sum.indexOf(max);
-            let x=value[index];
-            console.log(`Resulting array of digits  ${x}, less than k by ${max} `);
-            return x;
-        }
-    }
-    
-
-  
-//-----------------------------------start--from--ls[0],ls[1],ls[2]-digits--------------------------------------------------
-//1-st--element
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[0],ls[2]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,2,copied[2], copied[1]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[0],ls[3]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,3,copied[3], copied[2],copied[1]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[0],ls[4]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,4,copied[4], copied[2],copied[3],copied[1]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[2],ls[0]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,2,copied[1],copied[0]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[1],ls[2]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,2,copied[2],copied[1]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[1],ls[3]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,3,copied[3],copied[1],copied[2]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[1],ls[4]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,4,copied[4],copied[1],copied[2],copied[3]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[2],ls[0]-digits--------------------------------------------------
-//1-st--element
-copied.splice(0,5,copied[3],copied[4],copied[0],copied[1],copied[2]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[2],ls[1]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,2,copied[2],copied[1]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[2],ls[3]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,4,copied[4],copied[1],copied[2],copied[3]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[2],ls[4]-digits--------------------------------------------------
-//1-st--element
-copied.splice(1,4,copied[4],copied[1],copied[2],copied[3]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-
-//-----------------------------------start--from--ls[3],ls[0]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,4,copied[3],copied[0],copied[1],copied[2]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[3],ls[1]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,4,copied[3],copied[1],copied[0],copied[2]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[3],ls[2]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,4,copied[3],copied[2],copied[0],copied[1]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[3],ls[4]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,5,copied[3],copied[4],copied[0],copied[1],copied[2]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-
-//-----------------------------------start--from--ls[4],ls[0]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,5,copied[4],copied[0],copied[1],copied[2],copied[3]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[4],ls[1]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,5,copied[4],copied[1],copied[0],copied[2],copied[3]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[4],ls[2]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,5,copied[4],copied[2],copied[0],copied[1],copied[3]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-//-----------------------------------start--from--ls[4],ls[3]-digits--------------------------------------------------
-//1-st--element
-copied=ls.slice();
-copied.splice(0,5,copied[4],copied[3],copied[0],copied[1],copied[2]);
-SaveFirst(copied,preresult,result);
-//2-d--element
-getCombination(copied,k);
-
-
-return getFinalResult(result,t);
+return finalRest;
 }
 
-console.log(chooseDistance(174,k, ls ));
+function getFinalResult(value,t){
+    value.map((item)=>{
+        item=[].concat.apply([],item);
+        item=(item.reduce((a,b)=>a+b,0))-t;
+        sum.push(item);
+    });
+    let negative = sum.filter((elem)=>{
+        return elem<0;
+    });
+    if(negative.length==0){
+        console.log("There is no acceptable result");
+    }
+    else
+    {
+        let max=Math.max.apply(null, negative);
+        let index=sum.indexOf(max);
+        let x=value[index];
+        console.log(`Resulting array of digits  ${x}, less than k by ${max} `);
+        return x;
+    }
+}
+
+let chooseDistance = (t, k, ls) => {
+if(k>4){
+    console.log("There is no acceptable result");
+}    
+
+if(k==4){
+    function getFirstArg(i,p,w,k){
+        let argFirst,arg=[];
+        if(k==4){
+            argFirst=ls.slice(i,i+1);
+            argFirst=argFirst.concat(ls[p]);
+            argFirst=argFirst.concat(ls[w]);    
+        }
+        arg=arg.concat(argFirst);
+        return arg;    
+    }
+    
+    function getCombination(i,p,w,k){
+    
+        let o=getFirstArg(i,p,w,k);
+        console.log(o);
+        let n=getSecondArg(o);
+        console.log(n);
+        
+    
+        for(j=0;j<n.length;j++){
+            let semiResult=o.concat(n[j]);
+            result.push(semiResult);
+        }
+        return result;
+    }
+
+    
+    for(let c=0;c<ls.length;c++){
+        for(let a=0;a<ls.length;a++){
+            for(i=c,p=a,w=0;w<ls.length;w++){
+                if(a==i){
+                    console.log("we met a=i!");
+                    continue;
+                }
+                if(w==i){
+                    console.log("we met w=i!");
+                    continue;
+                }
+                if(w==p){
+                    console.log("we met w=p!");
+                    continue;
+                }
+                console.log(getCombination(i,p,w,4));    
+            }
+        }
+    }
+}
+else if(k==3){
+    function getFirstArg(i,p,k){
+        let argFirst,arg=[];
+        argFirst=ls.slice(i,i+1);
+        argFirst=argFirst.concat(ls[p]);
+        arg=arg.concat(argFirst);
+        return arg;    
+    }
+    
+    function getCombination(i,p,k){
+        let o=getFirstArg(i,p,k);
+        console.log(o);
+        let n=getSecondArg(o);
+        console.log(n);
+        
+    
+        for(j=0;j<n.length;j++){
+            let semiResult=o.concat(n[j]);
+            result.push(semiResult);
+        }
+        return result;
+    }
+    
+    for(let a=0;a<ls.length;a++){
+        for(i=a,p=0;p<ls.length;p++){
+            if(p==i){
+                console.log("we met w=i!");
+                continue;
+            }
+            else{
+                console.log(getCombination(i,p,3));    
+            }
+        }
+    }
+}
+else if(k==2){
+    function getFirstArg(i,k){
+        let argFirst,arg=[];
+        argFirst=ls.slice(i,i+1);
+        arg=arg.concat(argFirst);
+        return arg;    
+    }
+    function getCombination(i,k){
+        let o=getFirstArg(i,k);
+        console.log(o);
+        let n=getSecondArg(o);
+        console.log(n);
+
+        for(j=0;j<n.length;j++){
+            let semiResult=o.concat(n[j]);
+            result.push(semiResult);
+        }
+        return result;
+    } 
+    for(i=0;i<ls.length;i++){
+        console.log(getCombination(i,2));    
+    }  
+}
+return getFinalResult(result,t)
+}
 
 
+
+chooseDistance (174, 3, ls);
 
